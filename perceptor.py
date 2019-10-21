ef0a8dec3ea53cbc3d33d5f1acf59f34079e0f48
ikarrila <47640841+ikarrila@users.noreply.github.com>
2019-10-22 00:01:50 +0300

    Update perceptor.py

--- perceptor.py
+++ perceptor.py
@@ -1,27 +1,31 @@
 import numpy as np
 
+#how many training iterations, bigger number -> more precise results, but takes time
 loops = 1000
 
+#logistic function to put inputs in order
 def logistic(x):
     return 1 / (1 + np.exp(-x))
 
+#derivative of the logisctic function is used to determine how much weights ought to be adjusted
 def derivative(x):
     return x * (1 - x)
 
+#picking random weights, creating weight matrix
+weights = 2 * np.random.random((3,1)) - 1
+print('Starting weights: ')
+print(weights)
+
+#inputs and correct outputs
+training_outputs = np.array([[1,0,1,1,0]]).T
 inputs = np.array([[1,1,1],
                     [0,0,1],
                     [1,0,1],
                     [1,0,0],
                     [0,1,1]])
 
-training_outputs = np.array([[1,0,1,1,0]]).T
-
-weights = 2 * np.random.random((3,1)) - 1
-
-print('Starting weights: ')
-print(weights)
-
-for iteration in range(loops):
+#iteration loop where weights are adjusted depending on the error margin
+for i in range(loops):
     input_layer = inputs
     outputs = logistic(np.dot(input_layer, weights))
     error = training_outputs - outputs
